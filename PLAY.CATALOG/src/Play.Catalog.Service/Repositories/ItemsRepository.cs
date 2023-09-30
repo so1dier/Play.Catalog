55f2046cac3be63291792b4d8359ff0a60a08cab
Daniel Golitsynski <don.aus@gmail.com>
2023-09-30 23:21:30 +1000

    Added ItemsRepository class that interfaces with db storage

---
+++ PLAY.CATALOG/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -0,0 +1,61 @@
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+
+namespace Play.Catalog.Service.Repositories;
+
+public class ItemsRepository
+{
+    private const string collectionName = "items";
+    private readonly IMongoCollection<Item> dbCollection;
+    private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+    public ItemsRepository()
+    {
+        var mongoClient = new MongoClient("mondodb://localhost:27017");
+        var datavase = mongoClient.GetDatabase("Catalog");
+        dbCollection = datavase.GetCollection<Item>(collectionName);
+    }
+
+    public async Task<IReadOnlyCollection<Item>> GetAllAsync()
+    {
+        return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+    }
+
+    public async Task<Item> GetAsync(Guid id)
+    {
+        FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.id, id);
+        return await dbCollection.Find(filter).FirstOrDefaultAsync();
+    }
+
+    public async Task CraeateAsync(Item entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        await dbCollection.InsertOneAsync(entity);
+    }
+
+    public async Task UpdateAsync(Item entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        FilterDefinition<Item> filter = filterBuilder.Eq(existingEntity => existingEntity.id, entity.id);
+        await dbCollection.ReplaceOneAsync(filter, entity);
+    }
+
+    public async Task RemoveAsync(Guid id)
+    {
+        FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.id, id);
+        await dbCollection.DeleteOneAsync(filter);
+
+    }
+}
